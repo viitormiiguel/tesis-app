ab614e36061f2fc86ccdf99996f2e7a09fff7665
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-08-30 20:39:45 -0300

    testes

--- src/runPyfeat.py
+++ src/runPyfeat.py
@@ -4,6 +4,7 @@ from feat.data import Fex
 from feat.utils.io import get_test_data_path
 import matplotlib.pyplot as plt ## for visualization
 import os 
+import pandas as pd
 
 detector = Detector(
     face_model="retinaface",
@@ -17,17 +18,15 @@ detector = Detector(
 path = 'E:\\PythonProjects\\tesis-app\\input\\'
 
 # Get the full path
-single_face_img_path = os.path.join(path, "064_08.jpg")
+single_face_img_path = os.path.join(path, "099_08.jpg")
 
 single_face_prediction = detector.detect_image(single_face_img_path)
 
 # Show results
 # print(single_face_prediction.emotions)
 
-figs = single_face_prediction.plot_detections(poses=True, emotion_barplot=False, au_barplot=False)
+# figs = single_face_prediction.plot_detections(poses=True, emotion_barplot=False, au_barplot=False)
 
-figs2 = single_face_prediction.plot_detections(faces='aus', muscles=True, emotion_barplot=False, au_barplot=False)
-
-# figs2 = Fex.plot_detections(faces='aus', muscles=True, emotion_barplot=False, au_barplot=False)
+figs2 = single_face_prediction.plot_detections(faces='aus-heat', muscles=True, emotion_barplot=False, au_barplot=False)
 
 plt.show()
