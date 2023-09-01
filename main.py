0810790fb87e8dfbbaaa9212bbdd5fcc0f18f24b
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-09-01 17:40:48 -0300

    testes

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 
-from flask import Flask
+import time
+import os
+
+from flask import Flask, request
+
+from src.runOpenFace import runOpenFace
+from src.runOpenFace import saveData
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+## POST
+@app.route('/createData', methods=['POST'])
+def createData():
+    
+    request_body = request.get_json()
+    
+    data = {'file': request_body['file']}
+    
+    try:
+        
+        r = runOpenFace(request_body['file'])
+                
+        s = saveData(request_body['file'])
+            
+        return '## Dados salvos com sucesso! ##'
+        
+    except:
+        return '## Erro ao executar OpenFace ##'
+    
+    
+## GET 
+@app.route('/getData', methods=['GET'])
+def getData():
+    
+    try:
+        
+        print('teste')
+        
+    except:
+        return '## Erro ao executar OpenFace ##'
 
 if __name__ == '__main__':
+    
     app.run(host='0.0.0.0', port=105)
--- src/runOpenFace.py
+++ src/runOpenFace.py
@@ -1,5 +1,6 @@
 
 import os 
+import time
 import subprocess
 import shutil
 import csv
@@ -14,76 +15,86 @@ from os import path
 
 def runOpenFace(image):
     
-    nome = image.split('.')
-    nome = nome[0].split('\\')
-   
-    exec1 = path + image
-    exec2 = path + '\\output\\retOpen\\' + nome[1]
+    try:
+    
+        path = 'E:\\PythonProjects\\tesis-app'
+            
+        exec1 = path + '\\data\\input\\' + image
+        exec2 = path + '\\data\\output\\retOpen\\' + image
 
-    comando = ' -f ' + '"' +  exec1 + '"'
+        comando = ' -f ' + '"' +  exec1 + '"'
 
-    os.system('"C:\\OpenFace\\FaceLandmarkImg.exe' + comando + ' -out_dir ' + exec2 + '"')
-
-def readFile(image):
-    
-    nome = image.split('.')
-    nome = nome[0].split('\\')
-    
-    arq = path + 'output\\retOpen\\' + nome[1] + '\\' + nome[1] + '.csv'
-                    
-    arquivo = pd.read_csv(arq)
-    
-    valores = arquivo.iloc[:, 676:693]
-    
-    land2D = arquivo.iloc[:, 296:432]
+        os.system('"C:\\OpenFace\\FaceLandmarkImg.exe' + comando + ' -out_dir ' + exec2 + '"')
         
-    land3D = arquivo.iloc[:, 432:636]
-    
-    idImage = ObjectId(generate())
-    idOpen = ObjectId(generate())
-    
-    data = []    
-    data.append({
-        'imageId': idImage,
-        'personalId': 'teste',
-        'image': nome[1],
-        'imgeurl': '',
-        'type': 'real',
-    })
-    
-    dataOpen = []
-    dataOpen.append({
-        'openId': idOpen,
-        'imageId': idImage,
-        'aus': valores.to_json(orient = 'records'),
-        'landmarks2D': land2D.to_json(orient = 'records'),
-        'landmarks3D': land3D.to_json(orient = 'records'),
-    })
+        time.sleep(1)
+                
+        return '## Processo finalizado! ##'
         
-    return data, dataOpen
+    except:        
+        return '## Erro ao executar OpenFace ##'
+    
 
-def saveMOngo(ret):
-    
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["tesis-app"]
-    
-    ## Add image data
-    mycol = mydb["images"]
-    mycol.insert_many(ret[0])
+def saveData(image):
         
-    ## Add OpenFace Data Image
-    mycol = mydb["openFace"]
-    mycol.insert_many(ret[1])        
+    try:
+        
+        path = 'E:\\PythonProjects\\tesis-app'
+        
+        nome = image.split('.')
+    
+        arq = path + '\\data\\output\\retOpen\\' + image + '\\' + nome[0] + '.csv'
+                        
+        arquivo = pd.read_csv(arq)
+                
+        valores = arquivo.iloc[:, 676:693]
+        
+        land2D = arquivo.iloc[:, 296:432]
+            
+        land3D = arquivo.iloc[:, 432:636]
+        
+        idImage = ObjectId(generate())
+        idOpen = ObjectId(generate())
+        
+        data = []    
+        data.append({
+            'imageId': idImage,
+            'personalId': 'teste',
+            'image': nome[1],
+            'imgeurl': '',
+            'type': 'real',
+        })
+        
+        dataOpen = []
+        dataOpen.append({
+            'openId': idOpen,
+            'imageId': idImage,
+            'aus': valores.to_json(orient = 'records'),
+            'landmarks2D': land2D.to_json(orient = 'records'),
+            'landmarks3D': land3D.to_json(orient = 'records'),
+        })
+                
+        time.sleep(1)
+        
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb = myclient["tesis-app"]
+        
+        ## Add image data
+        mycol = mydb["images"]
+        mycol.insert_many(data)
+            
+        ## Add OpenFace Data Image
+        mycol = mydb["openFace"]
+        mycol.insert_many(dataOpen)   
+        
+        return '## Processo finalizado! ##'
+        
+    except:         
+        return '## Erro ao salvar no MongoDB ##'
 
 if __name__ == '__main__':
 
     path = 'E:\\PythonProjects\\tesis-app\\'
     
-    image = 'input\\58-12.jpg'
+    image = '064_08.jpg'
     
-    # runOpenFace(image)
-    
-    r = readFile(image)
-    
-    saveMOngo(r)
-    
+    readFile(image)
