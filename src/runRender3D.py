5c98b13a3763da6e1da557a7b5c70b874eb24d96
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-09-24 21:23:23 -0300

    testes

---
+++ src/runRender3D.py
@@ -0,0 +1,42 @@
+
+import os
+
+from simple_3dviz import Mesh
+from simple_3dviz.window import show
+from simple_3dviz.behaviours.movements import CameraTrajectory
+from simple_3dviz.behaviours.trajectory import Circle
+from simple_3dviz.utils import render
+from simple_3dviz.behaviours.io import SaveFrames
+
+def render3D(caminho):
+    
+    lista = os.listdir(caminho)
+    
+    for l in lista:
+        
+        print(l)
+        
+        if '.obj' in l:
+            print(caminho + l)
+            m = Mesh.from_file(caminho + l, color=(0.8, 0.8, 0.8, 1.0))
+            m.to_unit_cube()
+            
+            nome = l.split('.')            
+            print(nome)
+            render(
+                [m],
+                n_frames=200,
+                size=(256,256),
+                camera_position=(0.0, 0.15, 1.5),
+                up_vector=(0, 1, 0),
+                behaviours=[
+                    SaveFrames(caminho + nome[0] + '.png')
+                ]
+            )
+
+if __name__ == '__main__':
+    
+    path = './data/output/retDeep/'
+    
+    render3D(path)
+    
