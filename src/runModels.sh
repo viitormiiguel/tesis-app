82ab048e03d2c09fec304a89127244c6c999b10d
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-09-25 19:56:10 -0300

    testes

--- src/runModels.sh
+++ src/runModels.sh
@@ -1,5 +1,15 @@
 #/bin/bash
 
+## =========================== Deep3D
+
 cd ../../Deep3DFaceReconstruction/
 
 source demo.sh
+
+## =========================== DECA
+
+cd G:/PythonProjects/DECA/
+
+source demo.sh
+
+## =========================== EMOCA
