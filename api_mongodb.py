af7badb855433bc7d39db2c70e5f594d96271c61
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-10-02 17:51:13 -0300

    testes

--- api_mongodb.py
+++ api_mongodb.py
@@ -1,5 +1,5 @@
 
 from flask import Flask, request, jsonify
-from database import initialize_db
+from api_database import initialize_db
 
 
---
+++ src/runReports.py
@@ -0,0 +1,7 @@
+
+import csv
+from io import StringIO
+from fpdf import FPDF
+import pandas as pd
+
+
