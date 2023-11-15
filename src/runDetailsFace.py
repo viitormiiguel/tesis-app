b026ba7b8cd109bd5747ab90ec1cec6fd225e94f
viitormiiguel <vitor.peres@acad.pucrs.br>
2023-11-14 23:27:29 -0300

    testes

--- src/runDetailsFace.py
+++ src/runDetailsFace.py
@@ -10,18 +10,20 @@ from collections import OrderedDict
 
 from PIL import Image, ImageDraw
 
-def drawRegiona(imagem):
+def drawRegiona(imagem, valores, label):
 
     nome = imagem.split('.')
+    
+    listas = ['eye', 'month', 'nose', 'queixo']
 
     CHEEK_IDXS = OrderedDict(
         [
-            ("all", (21, 20, 19, 18, 17, 0, 1, 29, 15, 16, 26, 25, 24, 23, 22)),
+            valores
         ]
     )
 
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("./model/shape_predictor_68_face_landmarks.dat")
+    detector    = dlib.get_frontal_face_detector()
+    predictor   = dlib.shape_predictor("./model/shape_predictor_68_face_landmarks.dat")
 
     img = cv2.imread('./img/' + imagem)
     img = imutils.resize(img, width=600)
@@ -41,25 +43,31 @@ def drawRegiona(imagem):
                 pts[i] = [shape.part(j).x, shape.part(j).y]
 
             pts = pts.reshape((-1,1,2))
-
             
             cv2.polylines(overlay,[pts],True,(246,243,243),thickness = 2)
             # cv2.fillPoly(overlay, pts=[pts], color=(246,243,243))
         
         # cv2.imshow("Image", overlay)
-        cv2.imwrite('./img/' + nome[0] + '_detail.jpg', overlay)
-    #     cv2.waitKey(0)
-    #     if cv2.waitKey(1) & 0xFF == ord('q'): 
-    #         break
+        cv2.imwrite('./img/' + nome[0] + '_' + str(listas[label]) + '_detail.jpg', overlay)
 
-    # cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 
+    parts = [
+        ("eyes", (21, 20, 19, 18, 17, 0, 1, 29, 15, 16, 26, 25, 24, 23, 22)),
+        ("month", (3, 4, 5, 11, 12, 13, 35, 34, 33, 32, 31)),
+        ("nose", (3, 13, 14, 15, 28, 1, 2)),
+        ("queixo", (4, 5, 6, 7, 8, 9, 10, 11, 12, 54, 55, 56, 57, 58, 59, 60, 48))
+    ]
+
     lista = os.listdir('./img/')
+    
+    for i, j in enumerate(parts):
 
-    for l in lista:
+        for l in lista:
 
-        if 'detail' not in l:
+            if 'detail' not in l:
 
-            drawRegiona(l)
+                drawRegiona(l, j, i)
+                
+        # break
